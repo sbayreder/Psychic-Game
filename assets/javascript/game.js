e1eacaed349b6df2fc667c07a42828463fdd779a
sbayreder <sbayreder@gmail.com>
2020-02-15 09:31:43 -0600

    fixed js, still having issues with some js and some css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,30 +4,41 @@ var won=0;
 var lost=0;
 var attempts= 10;
 var usedLtr= [];
-var psyLetter= psyLetter;
 var letters=[ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
 
 
-//computer Psychic letter
- function randomChoice(){
-     return psyLetter=letters[Math.floor(Math.random()*letters.lenght)]
- 
-console.log(psyLetter);
- }
+//computer Psychic letter pick
+document.onkeyup=function(event){
+    var playerGuess=event.key;
+    var psychicGuess=letters[Math.floor(Math.random()* letters.length)];
+    console.log(psychicGuess)
+}
 
 
-//logging player input
+//if guess is correct
+if(letters.indexOf(playerGuess)>-1){
+    if (playerGuess===psychicGuess){
+        won++;
+        attempts=10;
+        usedLtr=[];
+    }
+    if (playerGuess!=psychicGuess){
+        attempts--;
+        usedLtr.push(playerGuess)
+    }
+}
 
 
 
-    //correct guess
- 
+//correct guess
 
-    //incorrect guess
- 
+
+//incorrect guess
+
+
 
 //when out of attempts
 
 
 //displaying content on HTML
-
+//onkeyup= document.getElementById("yourGuess").innerHTML = [usedLtr]
