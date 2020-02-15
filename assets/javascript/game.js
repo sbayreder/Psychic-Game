41e48c24211fc67bdb08e910198baa34bd8235e1
sbayreder <sbayreder@gmail.com>
2020-02-14 22:14:51 -0600

    html and css mostly complete, struggles with js

---
+++ assets/javascript/game.js
@@ -0,0 +1,33 @@
+//variables and counters
+
+var won=0;
+var lost=0;
+var attempts= 10;
+var usedLtr= [];
+var psyLetter= psyLetter;
+var letters=[ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
+
+
+//computer Psychic letter
+ function randomChoice(){
+     return psyLetter=letters[Math.floor(Math.random()*letters.lenght)]
+ 
+console.log(psyLetter);
+ }
+
+
+//logging player input
+
+
+
+    //correct guess
+ 
+
+    //incorrect guess
+ 
+
+//when out of attempts
+
+
+//displaying content on HTML
+
